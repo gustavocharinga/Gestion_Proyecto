fb44260ef2eba1b9e39ab8ee92bf6a525908245e
Gustavo Charinga <gustavo_ch93@hotmail.com>
2017-04-18 13:06:46 -0400

    Introduccion

---
+++ resources/views/Index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>TioVEN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <script type="applisalonion/x-javascript">
+         addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }
+    </script>
+    <link href="{{ asset('css/bootstrap.css') }}" rel='stylesheet' type='text/css'/>
+    <!-- Custom Theme files -->
+    <link href="{{ asset('css/Loader.css') }}" rel='stylesheet' type='text/css'/>
+
+
+    <!--/web-font-->
+    <link href='//fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Merriweather+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <!--/script-->
+
+
+</head>
+<body>
+
+
+
+<div id=preloader>
+  <!--  <img src="/../images/Imagen_De_Carga.jpg">-->
+    <div id=loader></div>
+</div>
+<div id="main">
+
+    <h2>PAPEL TIJERA</h2>
+</div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<!--<script src="../../public/js/jquery-3.2.1.min.js"></script>-->
+<script type="text/javascript">
+    $(window).load(function() {
+        $('#preloader').fadeOut('slow');
+        $('body').css({'overflow':'visible'});
+    })
+</script>
+
+
+
+
+
+</body>
+</html>
